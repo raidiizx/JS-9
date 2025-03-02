a95967a2f5c1a168777b59d689d41e8e10b916b6
平行 <Ak4tsuki.nice@gmail.com>
2025-03-02 17:28:54 +0700

    Intitial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from 'express'
+const app = express();
+const port = 3333;
+
+app.set('views', './views')
+app.set('view engine', 'hbs')
+
+app.get('/add', (req, res) => {
+    let a = parseInt(req.query.a);
+    let b = parseInt(req.query.b);
+    let c = a + b
+    res.render('demo', { a, b, c })
+})
+
+app.get('/items', async (req, res) => {
+    const items = await getSiteList();
+    const nsfw = (req.query.nsfw)
+    console.log(nsfw);
+    res.render('items', { items })
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+async function getSiteList() {
+    const response = await fetch('https://theindex.moe/api/items')
+    if (!response.ok) {
+        throw new console.error(`Error`);
+    }
+    const json = await response.json();
+    return json;
+}
+
